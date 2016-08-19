e54215f117081af6ea4bf79d04f1078671a4fded
s0lus <mashenkoalex@gmail.com>
2016-08-19 12:18:49 +0900

    Added config fo DB and connection to DB

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	_"github.com/jinzhu/gorm/dialects/postgres"
+	_"github.com/jinzhu/gorm"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+	"github.com/jinzhu/gorm"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	var config Config
+
+	jsonStream, error := ioutil.ReadFile("./config.json");
+	if error != nil {
+		fmt.Println("Error reading 'config.json' file")
+	}
+
+	json.Unmarshal(jsonStream, &config)
+
+	db, error := gorm.Open(config.DataBase.Dialect, config.DataBase.ConnectionData)
+	if error != nil {
+		fmt.Println("Error connection to database")
+	}
+
+	db.DB().SetMaxIdleConns(config.DataBase.IdleConnections)
+	db.DB().SetMaxOpenConns(config.DataBase.MaxOpenConnections)
+
+	db.LogMode(true);
+	db.AutoMigrate(&Image{})
+
 }
